e01c17ff9c181ceec163a5b487ee737c3841648f
Satonin Vyacheslav <satonin@lanit.ru>
2020-08-14 18:36:19 +0500

    попытка убрать кракозябру

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,6 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
+		System.out.println("Текст на русском в консоли");
 		return "Greetings from Spring Boot! from P.S. 5_printHelloWorld_maven_springboot";
 	}
 
